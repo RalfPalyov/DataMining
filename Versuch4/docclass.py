5bbca659168711f0b2c2a2b2d023ab8038b8e849
JanHorak <janhorak@gmx.de>
2013-11-18 12:04:11 +0100

    Bug fixed, Testcases added

--- Versuch4/docclass.py
+++ Versuch4/docclass.py
@@ -4,7 +4,9 @@ class Classifier:
     __cc = {'Good': 0, 'Bad': 0}
     __fc = {}
 
-    '''Constructor for Classifier-class
+
+    def __init__(self, fc, cc):
+        '''Constructor for Classifier-class
        --------------------------------
        The Classifier needs two dictionaries for the input.
 
@@ -21,74 +23,76 @@ class Classifier:
        --------
        -> Object
        '''
-    def __init__(self, fc, cc):
         self.__cc = cc
         self.__fc = fc
 
-    ''''''
+
     def incf(self, word, category):
         pass
 
-    '''Increases the count of the passed category at 1
+
+    def incc(self, category):
+        '''Increases the count of the passed category at 1
 
         Parameters to pass:
-       -------------------
-       category -> Keyvalue of the dictionary. Per Default "Good" or "Bad"
+        -------------------
+        category -> Keyvalue of the dictionary. Per Default "Good" or "Bad"
 
-       returns:
-       --------
-       void
-    '''
-    def incc(self, category):
+        returns:
+        --------
+        void'''
         newCount = self.__cc.get(category) +1
         self.__cc.update({category: newCount})
 
-    '''Returns the count of the passed word in the passed category
-        Parameters to pass:
-       -------------------
-       f -> A Word we want to check (Type: String)
-       category -> Keyvalue of the dictionary. Per Default "Good" or "Bad"
 
-       returns:
-       --------
-       Value of the count
-       (Integer)
-    '''
+
     def fcount(self, f, category):
+        '''Returns the count of the passed word in the passed category
+        Parameters to pass:
+        -------------------
+        f -> A Word we want to check (Type: String)
+        category -> Keyvalue of the dictionary. Per Default "Good" or "Bad"
+
+        returns:
+        --------
+        Value of the count (Integer)'''
         return self.__cc.get(category).get(f)
 
-    '''Returns the count of the passed category
-        Parameters to pass:
-       -------------------
-       category -> Keyvalue of the dictionary. Per Default "Good" or "Bad"
 
-       returns:
-       --------
-       Value of the count
-       (Integer)
-    '''
     def catcounts(self, category):
+        '''Returns the count of the passed category
+        Parameters to pass:
+        -------------------
+        category -> Keyvalue of the dictionary. Per Default "Good" or "Bad"
+
+        returns:
+        --------
+        Value of the count (Integer)'''
         return self.__cc.get(category)
 
-    '''Returns the total count of values Parameters to pass:
-        Parameters to pass:
-       -------------------
-       None
 
-       returns:
-       --------
-       Total count of both categories
-       (Integer)
-    '''
     def totalcount(self):
+        '''Returns the total count of values Parameters to pass:
+        Parameters to pass:
+        -------------------
+        None
+
+        returns:
+        --------
+        Total count of both categories(Integer)'''
         result = 0
-        for item, v in self.__cc:
-            result += v
+        for key in self.__cc.keys():
+            result += self.__cc.get(key)
         return result
+
+
 
     def train(self, item, category):
         pass
 
+
+
     def fprob(self, f, category):
         pass
+
 
